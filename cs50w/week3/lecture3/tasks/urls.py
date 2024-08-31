1d5fe033f774a6cd9b44e7c0804f46aac197440c
Lifeissimple-zxc <mararkarp@gmail.com>
2024-08-31 16:12:30 +0200

    another part of the lecture on django forms completed

--- cs50w/week3/lecture3/tasks/urls.py
+++ cs50w/week3/lecture3/tasks/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks" #  a unique identifier for routes
+
 urlpatterns = [
     path(route="", view=views.index, name="index"),
     path(route="add", view=views.add, name="add")
--- cs50w/week3/lecture3/tasks/views.py
+++ cs50w/week3/lecture3/tasks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from django import forms
 
 tasks = ["foo", "bar", "baz"]
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request: HttpRequest):
     return render(
@@ -15,5 +21,8 @@ def index(request: HttpRequest):
 def add(request: HttpRequest):
     return render(
         request=request,
-        template_name="tasks/add.html"
+        template_name="tasks/add.html",
+        context={
+            "form": NewTaskForm()
+        }
     )
