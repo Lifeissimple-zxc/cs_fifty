00196d53249c756a4ee72a4265e844c6a27d67df
Lifeissimple-zxc <mararkarp@gmail.com>
2024-09-01 13:31:03 +0200

    Completed lecture 3

--- cs50w/week3/lecture3/tasks/views.py
+++ cs50w/week3/lecture3/tasks/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpRequest
-from django import forms
+from django.http import HttpRequest, HttpResponseRedirect
+from django import forms, urls
 
-tasks = ["foo", "bar", "baz"]
+
+TASKS_KEY = "tasks"
 
 
 class NewTaskForm(forms.Form):
@@ -10,19 +11,33 @@ class NewTaskForm(forms.Form):
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request: HttpRequest):
+    # per user task list (session based)
+    if TASKS_KEY not in request.session:
+        request.session[TASKS_KEY] = []
     return render(
         request=request,
         template_name="tasks/index.html",
         context={
-            "tasks": tasks
+            "tasks": request.session[TASKS_KEY]
         }
     )
 
 def add(request: HttpRequest):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            request.session[TASKS_KEY] += [form.cleaned_data["task"]]
+            return HttpResponseRedirect(
+                urls.reverse(viewname="tasks:index")
+            )
+        return render(
+            request=request,
+            template_name="tasks/add.html",
+            context={"form": form}
+        )
+
     return render(
         request=request,
         template_name="tasks/add.html",
-        context={
-            "form": NewTaskForm()
-        }
+        context={"form": NewTaskForm()}
     )
