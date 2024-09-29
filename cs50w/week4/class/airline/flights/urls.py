4f603765fe4a71b5b2dd1d3cbb8d704c77681148
Lifeissimple-zxc <mararkarp@gmail.com>
2024-09-29 15:33:31 +0200

    added a POST method to map users to flights

--- cs50w/week4/class/airline/flights/urls.py
+++ cs50w/week4/class/airline/flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path(route="", view=views.index, name="index"),
-    path(route="<int:flight_id>", view=views.flight, name="flight")
+    path(route="<int:flight_id>", view=views.flight, name="flight"),
+    path(route="<int:flight_id>/book", view=views.book, name="book"),
 ]
--- cs50w/week4/class/airline/flights/views.py
+++ cs50w/week4/class/airline/flights/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request: HttpRequest):
@@ -21,6 +22,17 @@ def flight(request: HttpRequest, flight_id: int):
         context={
             "flight": f,
             # this is possible thanks to related_name attr
-            "passengers": f.passengers.all()
+            "passengers": f.passengers.all(),
+            # this performs a SELECT WHERE
+            "non_passengers": Passenger.objects.exclude(flights=f.id).all() 
         }
     )
+
+def book(request: HttpRequest, flight_id: int):
+    if request.method == "POST":
+        f = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(f)
+        return HttpResponseRedirect(
+            reverse(viewname="flight", args=(f.id,))
+        )
